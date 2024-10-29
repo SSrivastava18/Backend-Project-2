5ff664cd969900b52520ddf34f0baa33f8f08518
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-10-29 23:43:02 +0530

    models implemented

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const  express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,9 +10,20 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res)=>{
     res.render("index");
-})
-app.get('/read', (req,res)=>{
-    res.render("read");
-})
+});
+app.get('/read', async (req,res)=>{
+    let users = await userModel.find();
+    res.render("read", {users});
+});
+app.post('/create', async (req,res)=>{
+    let {name, email, image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+    res.send(createdUser);
+});
 
 app.listen(3000);
